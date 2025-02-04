0758feaa2f55528bed07e14807db1473daf7e15f
oliviamoran26 <olivia.moran@tmsw.com>
2025-02-04 13:06:57 -0600

    align text on card

--- src/components/Left.jsx
+++ src/components/Left.jsx
@@ -9,14 +9,28 @@ const Left = ({ recipe }) => {
       </h4>
       <h3>Instructions</h3>
       <h4>Step 1: {recipe.instructions.stepOne}</h4>
-      <h4>Step 2: {recipe.stepTwo}</h4>
-      {recipe.stepThree ? <h4>Step 3: {recipe.stepThree}</h4> : null}
-      {recipe.stepFour ? <h4>Step 4: {recipe.stepFour}</h4> : null}
-      {recipe.stepFive ? <h4>Step 5: {recipe.stepFive}</h4> : null}
-      {recipe.stepSix ? <h4>Step 6: {recipe.stepSix}</h4> : null}
-      {recipe.stepSeven ? <h4>Step 7: {recipe.stepSeven}</h4> : null}
-      {recipe.stepEight ? <h4>Step 8: {recipe.stepEight}</h4> : null}
-      {recipe.stepNine ? <h4>Step 9: {recipe.stepNine}</h4> : null}
+      <h4>Step 2: {recipe.instructions.stepTwo}</h4>
+      {recipe.instructions.stepThree ? (
+        <h4>Step 3: {recipe.instructions.stepThree}</h4>
+      ) : null}
+      {recipe.instructions.stepFour ? (
+        <h4>Step 4: {recipe.instructions.stepFour}</h4>
+      ) : null}
+      {recipe.instructions.stepFive ? (
+        <h4>Step 5: {recipe.instructions.stepFive}</h4>
+      ) : null}
+      {recipe.instructions.stepSix ? (
+        <h4>Step 6: {recipe.instructions.stepSix}</h4>
+      ) : null}
+      {recipe.instructions.stepSeven ? (
+        <h4>Step 7: {recipe.instructions.stepSeven}</h4>
+      ) : null}
+      {recipe.instructions.stepEight ? (
+        <h4>Step 8: {recipe.instructions.stepEight}</h4>
+      ) : null}
+      {recipe.instructions.stepNine ? (
+        <h4>Step 9: {recipe.instructions.stepNine}</h4>
+      ) : null}
     </div>
   )
 }
