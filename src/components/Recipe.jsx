ea2ef4e5eaf948e4194ff802adbfaa74728963f1
oliviamoran26 <olivia.moran@tmsw.com>
2025-02-03 21:13:06 -0600

    update 'problems' with json file

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -16,9 +16,9 @@ const Recipe = (props) => {
       <h3>Instructions</h3>
       <h4>Step 1: {props.stepOne}</h4>
       <h4>Step 2: {props.stepTwo}</h4>
+      {props.stepThree ? <h4>Step 3: {props.stepThree}</h4> : null}
       {props.stepFour ? <h4>Step 4: {props.stepFour}</h4> : null}
       {props.stepFive ? <h4>Step 5: {props.stepFive}</h4> : null}
-      {props.stepThree ? <h4>Step 3: {props.stepThree}</h4> : null}
       {props.stepSix ? <h4>Step 6: {props.stepSix}</h4> : null}
       {props.stepSeven ? <h4>Step 7: {props.stepSeven}</h4> : null}
       {props.stepEight ? <h4>Step 8: {props.stepEight}</h4> : null}
